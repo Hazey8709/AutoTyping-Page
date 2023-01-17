5cefa6191fc7e479eea6f97e667fd2537c013771
Hazey8709 <jfortin802@gmail.com>
2023-01-16 20:56:58 -0500

    first comm

---
+++ auto-type.js
@@ -0,0 +1,8 @@
+//  JavaScript
+
+var typed = new Typed(".auto-input", {
+    strings: ["Jordan", "a Web Developer in school for full stack development"],
+    typeSpeed: 75,
+    backspeed: 50,
+    loop: true,
+});
